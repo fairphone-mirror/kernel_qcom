207bac90cb897ab3f800d24affc691aef92f05f5
changyuan1.zhou <changyuan.zhou@t2mobile.com>
2025-01-18 16:44:32 +0800

    [FPS-619] [1/2]Temporary modification ESWIN TP driver update
    * Root Cause
      ESWIN tp bring up
    * Solution
      ESWIN tp bring up
    * Test Steps
      can build
    * Test Result
      tp can touch
    
    Change-Id: I453af4bb281de75cae0fd5492fb5ac25bef67229

--- drivers/spi/spi-msm-geni.c
+++ drivers/spi/spi-msm-geni.c
@@ -1608,9 +1608,10 @@ setup_ipc:
 	/* we should avoid reallocation of ipc context during deepsleep */
 	if (!mas->ipc)
 		mas->ipc = ipc_log_context_create(4, dev_name(mas->dev), 0);
-	dev_info(mas->dev, "tx_fifo %d rx_fifo %d tx_width %d\n",
+		/*
+	dev_dbg(mas->dev, "zxzspi tx_fifo %d rx_fifo %d tx_width %d\n",
 		mas->tx_fifo_depth, mas->rx_fifo_depth,
-		mas->tx_fifo_width);
+		mas->tx_fifo_width);*/
 	if (!mas->shared_ee)
 		mas->setup = true;
 
@@ -1778,8 +1779,9 @@ static int spi_geni_unprepare_transfer_hardware(struct spi_master *spi)
 
 		if (!mas->is_la_vm) {
 			/* Do this only for non TVM LA usecase */
-			ret = pinctrl_select_state(mas->geni_pinctrl,
-						mas->geni_gpio_sleep);
+			//ret = pinctrl_select_state(mas->geni_pinctrl,
+			//			mas->geni_gpio_sleep);
+			//printk("eph spi master pin set sleep\n");
 		}
 
 		if (ret)
@@ -2425,6 +2427,7 @@ static void spi_get_dt_property(struct platform_device *pdev,
 
 	geni_mas->dis_autosuspend =
 	of_property_read_bool(pdev->dev.of_node, "qcom,disable-autosuspend");
+	geni_mas->dis_autosuspend = true ;
 	/*
 	 * shared_se property is set when spi is being used simultaneously
 	 * from two Execution Environments.
